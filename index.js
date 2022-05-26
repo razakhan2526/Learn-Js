8084394f4b064b267f213c1520ce31a0384ca69c
razakhan2526 <razagulkhan62@gmail.com>
2022-05-26 20:21:09 +0500

    new

--- index.js
+++ index.js
@@ -90,12 +90,76 @@ At a very high level, there are two types of data types in javascript
 // console.log(!false);
 
 // Fuction in Javascript
-// DRY = Do not repeat yourself
-function avg(a, b){
-    c = (a + b)/2;
+function avg(a, b) {
+    c = (a + b) / 2;
     return c;
 }
 
+// DRY = Do not repeat yourself
 c1 = avg(4, 6);
 c2 = avg(14, 16);
-console.log(c1, c2);
+// console.log(c1, c2);
+
+// Conditionals in JavaScript
+var age = 30;
+// Single if statement
+// if (age > 18) {
+//     console.log('You can drink water');
+// }
+
+// if - else statement
+// if(age > 18){
+//     console.log('You can drink water');
+// }
+
+// else{
+//     console.log('You cannot drink water');
+// }
+/*
+// if-else ladder 
+if(age > 32){
+    console.log("You are not a Kid");
+}
+else if(age > 26){
+    console.log("bachhe nhi rahe");
+}
+else if(age > 22){
+    console.log("Yes bachhe nhi rahe");
+}
+else if(age > 18){
+    console.log("18 bachhe nhi rahe");
+}
+else{
+    console.log("bachhe rahe");
+}
+console.log("End Of Ladder");
+*/
+
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+ console.log(arr);
+ for(var i = 0;i<arr.length;i++){
+     if(i==2){
+         break
+         continue;
+     }
+     console.log(arr[i]);
+ }
+
+//  arr.forEach(function(element){
+//      console.log(element);
+//  });
+
+// const ac = 0;
+// ac++;
+// ac  =  ac + 1; 
+let j = 10;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
+
+do{
+    console.log(arr[j]);
+    j++;
+}while(j < arr.length);
